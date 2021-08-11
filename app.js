77ff022eb7984b4d459f6f863ea7fd210a343ff0
Arpita1899 <arpitaverma8885@gmail.com>
2021-08-12 00:01:31 +0530

    updated functionality

--- app.js
+++ app.js
@@ -1,9 +1,56 @@
-function reverseStr(){
+function reverseStr(str){
+    var listOfChars=str.split('');
+    var reversedListOfChars=listOfChars.reverse();
+    var reverseStr=reversedListOfChars.join('');
 
+return reverseStr;
+// return str.split('').reverse().join('');
+// shorter way
 }
-function isPalindrome(){
 
+function isPalindrome(str){
+var reverse=reverseStr(str);
+return str===reverse;
 }
-function getAllDateFormats(){
 
+
+function convertDateToString(date){
+
+var dateInString= { day:'',month:'',year:''};
+if(date.day<10){
+    dateInString.day="0"+date.day;
 }
+else{
+    dateInString=date.day.toString();
+}
+if(date.month<10){
+    dateInString.month="0"+date.month;
+}
+else{
+    dateInString.month=date.month.toString();
+}
+dateInString.year=date.year.toString();
+return dateInString;
+}
+
+
+
+function getAllDateFormats(date){
+    var dateStr=convertDateToString(date);
+
+    var ddmmyyyy=dateStr.day+dateStr.month+dateStr.year;
+    var mmddyyyy=dateStr.month+dateStr.day+dateStr.year;
+    var yyyymmdd=dateStr.year+dateStr.month+dateStr.day;
+    var ddmmyy=dateStr.day+dateStr.month+dateStr.year.slice(-2);
+    var mmddyy=dateStr.month+dateStr.day+dateStr.year.slice(-2);
+    var yymmdd=dateStr.year.slice(-2)+dateStr.month+dateStr.day;
+
+    return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd];
+}
+var date={
+    day:5,
+    month:9,
+    year:2020
+}
+
+console.log(getAllDateFormats(date));
