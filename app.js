aabe395339851ba19cd6187f113ba739bd32e8e9
Arpita1899 <arpitaverma8885@gmail.com>
2021-08-12 01:06:11 +0530

    js

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ return str===reverse;
 function convertDateToString(date){
 
 var dateInString= { day:'',month:'',year:''};
-if(date.day<10){
+if(date.day < 10){
     dateInString.day="0"+date.day;
 }
 else{
     dateInString=date.day.toString();
 }
-if(date.month<10){
+if(date.month <10){
     dateInString.month="0"+date.month;
 }
 else{
@@ -41,16 +41,102 @@ function getAllDateFormats(date){
     var ddmmyyyy=dateStr.day+dateStr.month+dateStr.year;
     var mmddyyyy=dateStr.month+dateStr.day+dateStr.year;
     var yyyymmdd=dateStr.year+dateStr.month+dateStr.day;
-    var ddmmyy=dateStr.day+dateStr.month+dateStr.year.slice(-2);
+    var ddmmyy=dateStr.day+dateStr.month+ dateStr.year.slice(-2);
     var mmddyy=dateStr.month+dateStr.day+dateStr.year.slice(-2);
     var yymmdd=dateStr.year.slice(-2)+dateStr.month+dateStr.day;
 
     return [ddmmyyyy,mmddyyyy,yyyymmdd,ddmmyy,mmddyy,yymmdd];
 }
-var date={
-    day:5,
-    month:9,
-    year:2020
+
+
+
+function checkPalindromeForAllDateFormats(date){
+    var listOfPalindromes=getAllDateFormats(date);
+    var palindrome=false;
+    for(var i=0;i<listOfPalindromes.length;i++){
+        if(isPalindrome(listOfPalindromes[i])){
+            palindrome=true;
+            break;
+        }
+    }
+    return palindrome;
 }
 
-console.log(getAllDateFormats(date));
+function leapYear(year){
+    if(year%400===0){
+        return true;
+}
+if(year%100===0){
+    return false;
+}
+if(year%4===0){
+    return true;
+}
+    return false;
+}
+
+function getNextDate(date){
+    var day=date.day+1;
+    var month=date.month;
+    var year=date.year;
+    var daysInMonth=[31,28,31,30,31,30,31,31,30,31,30,31];
+
+    if(month===2){
+        if(leapYear(year)){
+            if(day>29){
+                day=1;
+                month++;
+            }
+        }
+            else{
+                if(day>28){
+                    day=1;
+                    month++;
+                }
+            }
+        
+
+    }
+    else{
+        if(day>daysInMonth[month-1]){
+            day=1;
+            month++;
+        }
+    }
+
+    if(month>12){
+        month=1;
+        year;
+    }
+
+    return {
+        day:day,
+        month:month,
+        year:year
+    };
+
+}
+
+function getNextPalindromeDate(date){
+    var ctr=0;
+    var nextDate=getNextDate(date);
+    while(1){
+        ctr++;
+    var isPalindrome=checkPalindromeForAllDateFormats(nextDate);
+        if(isPalindrome){
+            break;
+        }
+        nextDate=getNextDate(nextDate);
+    }
+
+    return [ctr, nextDate];
+    
+}
+
+var date={
+    day:28,
+    month:2,
+    year:2020
+};
+
+console.log(getNextPalindromeDate(date));
